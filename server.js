a78257c35595cfc9729fe685205b4393c7f235b8
unknowncyber256 <166958496+unknowncyber256@users.noreply.github.com>
2026-06-13 07:37:08 +0300

    Implement HTTP to HTTPS proxy server

---
+++ server.js
@@ -0,0 +1,33 @@
+const https = require('https');
+const http = require('http');
+
+http.createServer((req, res) => {
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200, {'Access-Control-Allow-Origin':'*','Access-Control-Allow-Methods':'GET,POST,OPTIONS,PUT,DELETE','Access-Control-Allow-Headers':'*'});
+    res.end();
+    return;
+  }
+  const targetHost = req.headers['x-target-host'] || 'munowatch.org';
+  const headers = {...req.headers};
+  delete headers['x-target-host'];
+  delete headers['host'];
+  delete headers['connection'];
+  const options = {
+    hostname: targetHost, port: 443,
+    path: req.url, method: req.method,
+    headers: {...headers, host: targetHost}
+  };
+  let body = [];
+  req.on('data', chunk => body.push(chunk));
+  req.on('end', () => {
+    const proxy = https.request(options, r => {
+      const h = {...r.headers, 'access-control-allow-origin':'*', 'access-control-allow-credentials':'true'};
+      delete h['transfer-encoding'];
+      res.writeHead(r.statusCode, h);
+      r.pipe(res);
+    });
+    proxy.on('error', e => { res.writeHead(500); res.end(e.message); });
+    if (body.length) proxy.write(Buffer.concat(body));
+    proxy.end();
+  });
+}).listen(process.env.PORT || 3000, () => console.log('Proxy running'));
